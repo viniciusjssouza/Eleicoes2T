e67a7cd7d8fedb3e9ffca6fc8e445fd02fbb1373
viniciusjssouza <viniciusjssouza@gmail.com>
2019-01-06 20:54:10 -0200

    apply autoencoder to compress input

--- input.py
+++ input.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+
+def read_sample_dataset():
+    return pd.read_csv('data/eleicoes-twitter-small.csv', delimiter=';')
+
+def read_full_dataset():
+    return pd.read_csv('data/eleicoes-twitter.csv', delimiter=';')
